f467ee962b5420472a4a9742017cb03c30dad691
jackytu <zhongying.tu@gmail.com>
2014-04-06 12:20:20 +0800

    test hook

--- example.py
+++ example.py
@@ -2,10 +2,12 @@ import sys, time
 sys.path.append("/Users/tuzhongying/Tools/homebrew/lib/python2.7/site-packages")
 from nats.client import NatsClient
 
+NATS_URI = "nats://n_user:n_passwd@10.36.166.46:8446"
+
 def main():
     nats = NatsClient()
     conn, error  = nats.connect_nats({
-        "uri" : "nats://n_user:n_passwd@10.36.166.46:8446"}
+        "uri" : NATS_URI }
     )
     if error: sys.exit(-1)
 
