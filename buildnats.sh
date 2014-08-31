67b468c5c3f2cda35a76af2024909a0ba354fa29
jackytu <zhongying.tu@gmail.com>
2014-08-31 11:19:45 +0800

    mv nats-server to vendor

--- buildnats.sh
+++ buildnats.sh
@@ -9,8 +9,8 @@ fi
 
 echo "Using NATS version $NATS_VERSION"
 
-git clone https://github.com/derekcollison/nats.git
-cd nats
+git clone https://github.com/derekcollison/nats.git vendor/nats-server
+cd vendor/nats-server
 git checkout $NATS_VERSION
 
 ${TRAVIS:?"This is not a Travis build. All Done"}
